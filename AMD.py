c58d5ab95251aa843f75397e78f1b9f13f761120
Jae Jung <124737072+leojkj@users.noreply.github.com>
2024-06-06 16:36:42 -0400

    Update AMD.py

--- AMD.py
+++ AMD.py
@@ -183,3 +183,94 @@ plt.ylabel('Cumulative Return')
 plt.grid(True)
 plt.show()
 
+#Forecasting
+#Analyzing the close price trend again
+plt.plot(amd.index, amd['Close'])
+start = dt.datetime(2019, 6, 3)
+end = dt.datetime(2022, 6, 3)
+
+training_data = amd.loc[start:end]
+print(training_data.head())
+
+#preparing my data
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(training_data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#model
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+#testing the model accuracy on existing data
+
+test_start = dt.datetime(2022, 6, 4)
+test_end = dt.datetime(2024, 5, 30)
+
+test_data = amd.loc[test_start:test_end, 'Close']
+actual_prices = amd['Close'].values
+
+total_dataset = pd.concat((amd['Close'], test_data), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+#prediction with test data
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+x_test = np.array(x_test)
+if len(x_test.shape) == 2:
+    x_test = np.reshape(x_test,(x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+plt.plot(actual_prices_for_plot, color="green", label="Actual Prices")
+
+# Plot the predicted prices
+plt.plot(predicted_prices, color="red", label="Predicted Prices")
+
+plt.title("Actual Prices and Predicted Prices")
+plt.xlabel('Time')
+plt.ylabel('AMD Share Price')
+plt.legend()
+plt.show()
+
+#predicting next day
+
+real_data = [model_inputs[len(model_inputs) - prediction_days:len(model_inputs), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(prediction)
+
+#next day 6/1/24 predicted 141.66 
